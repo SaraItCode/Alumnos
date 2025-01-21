39f6407872cb0d4d0ccc33a5f32b7e961fa7704d
Sara <saraortega.docente@gmail.com>
2025-01-21 13:50:59 +0100

    Add images and change style. addEventListener button marcar_asistencia

--- public/add_alumno.js
+++ public/add_alumno.js
@@ -2,7 +2,7 @@ document.getElementById('add-alumno-form').addEventListener('submit', async (e) 
     e.preventDefault();
     const formData = new FormData(e.target);
     const newAlumno = {
-      id: Date.now(),
+      id: Date.now(), // Genera el id en funcion de la hora actual
       name: formData.get('name'),
       photo: formData.get('photo').name, // Enviar nombre del archivo para simplificar
       details: formData.get('details'),
